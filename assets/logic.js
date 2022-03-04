f72552249c9bb839157e99d3a329821b9f502f63
julia <julia@horusys.com>
2022-03-04 12:58:12 +0200

    copy theme

--- assets/logic.js
+++ assets/logic.js
@@ -44,7 +44,7 @@ function clearSearch(event) {
 }
 
 function onScroll() {
-  var content = document.getElementById("body");
+  var content = document.getElementById("content");
   var yOffset = content.scrollTop;
   console.log(yOffset)
   if (yOffset > 300) {
