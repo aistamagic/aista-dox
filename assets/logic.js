5342b81729ccc51cd561127bd73412c4b7464868
julia <julia@horusys.com>
2022-03-04 08:52:46 +0200

    copy theme

--- assets/logic.js
+++ assets/logic.js
@@ -1,10 +1,13 @@
 
 function toggleMenu(event) {
+  var body = document.getElementById('body');
   var banner = document.getElementById('banner');
   if (banner.className == 'banner') {
     banner.className = 'banner expanded';
+    body.style.overflow = 'hidden';
   } else {
     banner.className = 'banner';
+    body.style.overflow = '';
   }
   event.preventDefault();
 }
@@ -13,8 +16,8 @@ function searchFn() {
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("searchInput");
   filter = input.value.toLowerCase();
-  ul = document.getElementById("nav");
-  li = ul.getElementsByTagName("li");
+  ul = site.nav_sections;
+  li = ul.items;
   for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
       txtValue = a.textContent || a.innerText;
