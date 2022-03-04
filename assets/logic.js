576655cdd1d10a615622b907c7dec8c871e41f64
julia <julia@horusys.com>
2022-03-04 08:42:18 +0200

    copy theme

--- assets/logic.js
+++ assets/logic.js
@@ -8,3 +8,21 @@ function toggleMenu(event) {
   }
   event.preventDefault();
 }
+
+function searchFn() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toLowerCase();
+  ul = document.getElementById("nav");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toLowerCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+  event.preventDefault();
+}
