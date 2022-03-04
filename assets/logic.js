b85226904dd2b231f3d3b2db01323c73793d33ed
julia <julia@horusys.com>
2022-03-04 09:39:36 +0200

    copy theme

--- assets/logic.js
+++ assets/logic.js
@@ -15,17 +15,17 @@ function toggleMenu(event) {
 function searchFn(event) {
 
   var input  = document.getElementById('searchInput'),
-      filter = input.value.toUpperCase(),
+      filter = input.value.toLowerCase(),
       li     = document.querySelectorAll("ul#nav li");
 
   // Loop through all list items, and hide those who don't match the search query
   for (var i = 0; i < li.length; i++) {
     var a = li[i].querySelector("a");
 
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
       li[i].style.backgroundColor = 'rgb(3, 169, 244, 0.25)';
     } else {
-      li[i].style.backgroundColor = "";
+      li[i].style.backgroundColor = "transparent";
     }
   }
   event.preventDefault();
